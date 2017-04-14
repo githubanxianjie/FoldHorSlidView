cfea0c4bc1e0281cfc45177c71d5ff74cd05dc41
anxianjie <anxianjie362744930@gmail.com>
2017-04-14 14:46:16 +0800

    update3

--- app/src/main/java/com/foldhorslidview/main/MainActivity.java
+++ app/src/main/java/com/foldhorslidview/main/MainActivity.java
@@ -19,7 +19,7 @@ import comm.custom.RelativeLayoutViewPager;
 public class MainActivity extends AppCompatActivity {
     private RelativeLayoutViewPager view_pager_parent;
     private CustomViewPager cvp_view_pager;
-    private CustomViewPagerAdapter pagerAdapter;//1224
+    private CustomViewPagerAdapter pagerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
